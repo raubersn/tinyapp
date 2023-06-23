db95fda693f4156cf9a8ee9e5da60b5cafc214ae
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-06-22 23:57:58 -0700

    Build a web form and send the form data to a backend API that creates shortened URLs

--- express_server.js
+++ express_server.js
@@ -1,13 +1,39 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
-app.set("view engine", "ejs");
+const SHORT_URL_LENGTH = 6;
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomCharCode() {
+  const min = 48; // 0 (ASCII code)
+  const max = 122; // z (ASCII code)
+
+  let charCode = Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+
+  //eliminating invalid values
+  if (((charCode > 57) && (charCode < 65)) || ((charCode > 90) && (charCode < 97))) {
+    charCode = generateRandomCharCode();
+  }
+  
+  return (charCode);
+}
+
+const generateRandomString = () => {
+  let charCodes = '';
+
+  while (charCodes.length < SHORT_URL_LENGTH) {
+    charCodes += String.fromCharCode(generateRandomCharCode());
+  }
+
+  return (charCodes);
+};
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -33,6 +59,17 @@ app.get("/fetch", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  urlDatabase[generateRandomString()] = req.body.longURL;
+
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:id", (req, res) => {
