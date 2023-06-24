9adbd4b5a08cf153e3d341d931b608346f51466b
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-06-24 01:39:41 -0700

    Use cookie-parser to have a username displayed once a user is logged in

--- express_server.js
+++ express_server.js
@@ -1,5 +1,12 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
+
 const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 const PORT = 8080; // default port 8080
 const SHORT_URL_LENGTH = 6;
 
@@ -32,8 +39,7 @@ const generateRandomString = () => {
   return (charCodes);
 };
 
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -56,8 +62,13 @@ app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
 });
 
+//serious stuff
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"]
+  };
+
   res.render("urls_index", templateVars);
 });
 
@@ -70,11 +81,20 @@ app.post("/urls", (req, res) => {
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    username: req.cookies["username"]
+  };
+  
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id],
+    username: req.cookies["username"]
+   };
+
   res.render("urls_show", templateVars);
 });
 
