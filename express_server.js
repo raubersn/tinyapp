c72463f64a968518ef418be420f625725294d6c4
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-06-23 23:17:58 -0700

    Implement a DELETE operation to remove existing shortened URLs from our database

--- express_server.js
+++ express_server.js
@@ -78,6 +78,12 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+
+  res.redirect("/urls");
+});
+
 app.get("/u/:id", (req, res) => {
   res.redirect(urlDatabase[req.params.id]);  
 });
