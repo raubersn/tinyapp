34f7f7efe635bb085306ab0c1b02c0e7f8015d92
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-06-30 22:24:09 -0700

    Add features to control who has access to viewing, editing, and deleting URLs

--- express_server.js
+++ express_server.js
@@ -10,10 +10,27 @@ app.use(cookieParser());
 const PORT = 8080; // default port 8080
 const SHORT_URL_LENGTH = 6;
 const COOKIE_USER_ID = "user_id";
+const ACCESS_DENIED = "Access denied! This feature is only available to registered users.";
+const NO_PRIVILEGES = "Access denied! You do not have privileges over this TinyURL.";
+const UNEXISTING_URL = "The TinyURL informed does not exists.";
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: "admin"
+  },
+  "9sm5xK": {
+    longURL: "http://www.google.com",
+    userID: "admin"
+  },
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
 };
 
 const users = {
@@ -35,6 +52,19 @@ const getUserByEmail = (email) => {
   }
 
   return (null);
+};
+
+//returns the URLs where the userID is equal to the id of the currently logged-in user.
+const urlsForUser = (id) => {
+  const userURL = {};
+
+  for (let url in urlDatabase) {
+    if (urlDatabase[url].userID === id) {
+      userURL[url] = urlDatabase[url];      
+    }
+  }
+
+  return (userURL);
 };
 
 const generateRandomCharCode = () => {
@@ -86,21 +116,27 @@ app.get("/fetch", (req, res) => {
 
 //serious stuff
 app.get("/urls", (req, res) => {
-  const templateVars = { 
-    urls: urlDatabase,
-    user: users[req.cookies[COOKIE_USER_ID]]
-  };
+  if (!req.cookies[COOKIE_USER_ID]) {
+    res.status(403).send(ACCESS_DENIED);
+  } else {
+    const templateVars = { 
+      urls: urlsForUser(req.cookies[COOKIE_USER_ID]),
+      user: users[req.cookies[COOKIE_USER_ID]]
+    };
 
-  res.render("urls_index", templateVars);
+    res.render("urls_index", templateVars);
+  }
 });
 
 app.post("/urls", (req, res) => {
   if (!req.cookies[COOKIE_USER_ID]) {
-    res.status(403).send("Access denied! This feature is available only to registered users.");
+    res.status(403).send(ACCESS_DENIED);
   } else {
     const tinyId = generateRandomString();
 
-    urlDatabase[tinyId] = req.body.longURL;
+    urlDatabase[tinyId] = {};
+    urlDatabase[tinyId].longURL = req.body.longURL;
+    urlDatabase[tinyId].userID = req.cookies[COOKIE_USER_ID];
 
     res.redirect("/urls/" + tinyId);
   }
@@ -109,7 +145,7 @@ app.post("/urls", (req, res) => {
 app.get("/urls/new", (req, res) => {
   if (!req.cookies[COOKIE_USER_ID]) {
     res.redirect("/login");
-  } else {    
+  } else {
     const templateVars = { 
       user: users[req.cookies[COOKIE_USER_ID]]
     };
@@ -119,32 +155,56 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { 
-    id: req.params.id, 
-    longURL: urlDatabase[req.params.id],
-    user: users[req.cookies[COOKIE_USER_ID]]
-   };
+  if (urlDatabase[req.params.id] === undefined) {
+    res.status(400).send(UNEXISTING_URL);
+  } else if (!req.cookies[COOKIE_USER_ID]) {
+    res.status(403).send(ACCESS_DENIED);
+  } else if (urlDatabase[req.params.id].userID !== req.cookies[COOKIE_USER_ID]) {
+    res.status(403).send(NO_PRIVILEGES);
+  }  else {
+    const templateVars = { 
+      id: req.params.id, 
+      longURL: urlDatabase[req.params.id].longURL,
+      user: users[req.cookies[COOKIE_USER_ID]]
+    };
 
-  res.render("urls_show", templateVars);
+    res.render("urls_show", templateVars);
+  }
 });
 
 app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id] = req.body.longURL;
-
-  res.redirect("/urls");
+  if (urlDatabase[req.params.id] === undefined) {
+    res.status(400).send(UNEXISTING_URL);
+  } else if (!req.cookies[COOKIE_USER_ID]) {
+    res.status(403).send(ACCESS_DENIED);
+  } else if (urlDatabase[req.params.id].userID !== req.cookies[COOKIE_USER_ID]) {
+    res.status(403).send(NO_PRIVILEGES);
+  }  else {
+    urlDatabase[req.params.id].longURL = req.body.longURL;
+  
+    res.redirect("/urls");
+  }
 });
 
 app.post("/urls/:id/delete", (req, res) => {
-  delete urlDatabase[req.params.id];
+  if (urlDatabase[req.params.id] === undefined) {
+    res.status(400).send(UNEXISTING_URL);
+  } else if (!req.cookies[COOKIE_USER_ID]) {
+    res.status(403).send(ACCESS_DENIED);
+  } else if (urlDatabase[req.params.id].userID !== req.cookies[COOKIE_USER_ID]) {
+    res.status(403).send(NO_PRIVILEGES);
+  }  else {
+    delete urlDatabase[req.params.id];
 
-  res.redirect("/urls");
+    res.redirect("/urls");
+  }
 });
 
 app.get("/u/:id", (req, res) => {
   if (urlDatabase[req.params.id]) {
-    res.redirect(urlDatabase[req.params.id]);
+    res.redirect(urlDatabase[req.params.id].longURL);
   } else {
-    res.status(400).send("The TinyURL informed does not exists.");
+    res.status(400).send(UNEXISTING_URL);
   }
   
 });
